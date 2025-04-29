3afc41155402eabc286a0cb2c9536aa68ec303b4
mrnaveenwoodworks <mrnaveenwoodworks@gmail.com>
2025-04-29 13:56:47 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>E-Commercew</title>
+        <title>Netlify deploy</title>
         <meta
           name="description"
           content="Your ultimate destination for effortless online shopping. Discover curated collections, easily add items to your cart and wishlist,and enjoy detailed product descriptions with captivating previews. Experience convenience like never before with our intuitive interface. Shop smarter with us today."
